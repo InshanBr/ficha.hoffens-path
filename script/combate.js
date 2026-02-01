a717956f828dbfb99e57c8b3aa69dd700947c5a1
Iury-AB <iuryalves.iab@gmail.com>
2026-02-01 16:38:03 -0300

    correção no subtrairVida

--- script/combate.js
+++ script/combate.js
@@ -5,9 +5,18 @@ function subtrairVida(dano, pctEstamina) {
   const vidaAtual = document.getElementById("vida-combate");
   const pct = Number(document.getElementById(pctEstamina).value) || 0;
 
+  const subtrairRes = document.getElementById("sub-res");
+
   if(danoSofrido > resistencia) {
-    vidaAtual.value = vidaAtual.value - danoSofrido + resistencia;
-    reducao = Math.floor((danoSofrido - resistencia) * pct/100);
+    vidaAtual.value = Number(vidaAtual.value) - danoSofrido;
+
+    if(subtrairRes.checked){
+      vidaAtual.value = Number(vidaAtual.value) + resistencia;
+      reducao = Math.floor((danoSofrido - resistencia) * pct/100);
+    } else {
+      reducao = Math.floor((danoSofrido) * pct/100);
+    }
+    
   }
   else{
     reducao = 0;
@@ -29,4 +38,83 @@ function calculoEsforcoExtra() {
 
   document.getElementById("esforco-20").value = Math.floor(estamina*0.2);
   document.getElementById("esforco-50").value = Math.floor(estamina*0.5);
+}
+
+function determinarRolagem(thisDano) {
+  var rolagem=1;
+
+  if (thisDano == 0) {
+    rolagem = 1;
+  }
+  else if (thisDano == 1){
+    rolagem = "1d4+1";
+  }
+  else if (thisDano == 2){
+    rolagem = "2d4+2";
+  }
+  else if (thisDano == 3){
+    rolagem = "3d4+3";
+  }
+  else if (thisDano == 4){
+    rolagem = "3d6+4";
+  }
+  else if (thisDano == 5){
+    rolagem = "4d6+5";
+  }
+  else if (thisDano == 6){
+    rolagem = "4d8+6";
+  }
+  else if (thisDano == 7){
+    rolagem = "5d8+7";
+  }
+  else if (thisDano == 8){
+    rolagem = "5d10+8";
+  }
+  else if (thisDano == 9){
+    rolagem = "6d10+9";
+  }
+  else if (thisDano == 10){
+    rolagem = "6d12+10";
+  }
+  else if (thisDano == 11){
+    rolagem = "7d12+11";
+  }
+  else if (thisDano == 12){
+    rolagem = "8d12+12";
+  }
+  else if (thisDano == 13){
+    rolagem = "9d12+13";
+  }
+  else if (thisDano == 14){
+    rolagem = "10d12+14";
+  }
+  else if (thisDano == 15){
+    rolagem = "7d20+15";
+  }
+  else if (thisDano == 16){
+    rolagem = "8d20+16";
+  }
+  else if (thisDano == 17){
+    rolagem = "9d20+17";
+  }
+  else if (thisDano == 18){
+    rolagem = "10d20+18";
+  }
+  else if (thisDano == 19){
+    rolagem = "11d20+19";
+  }
+  else if (thisDano == 20){
+    rolagem = "12d20+20";
+  }
+  else {
+    rolagem = "---";
+  }
+  return rolagem;
+}
+
+function atribuirRolagemDano(nvl, rolagem) {
+  const nivelDano = Number(document.getElementById(nvl).value) || 0;
+  const rolagemCampo = document.getElementById(rolagem);
+  
+  rolagemCampo.value = determinarRolagem(nivelDano);
 }
