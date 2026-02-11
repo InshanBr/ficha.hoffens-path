0dac601009a7220d42045132058cd0f850c660ef
Iury-AB <iuryalves.iab@gmail.com>
2026-02-11 09:35:26 -0300

    correção modificadores 2

--- script/poder_vantagens.js
+++ script/poder_vantagens.js
@@ -494,12 +494,12 @@ function adicionarModificadores(poder, efeito, ligado) {
   novaLinha.innerHTML = `
     <br>
     <input type="number" id="custo-modificador-${poder}-${efeito}-${ligado}-${contadorModificadores[poder][efeito][ligado]}" name="custo-modificador" class="dependente" placeholder="Custo">
-    <select name="tipo-modificador" id="tipo-modificador-${poder}-${efeito}-${ligado}-${contadorModificadores[poder][efeito]}" class="dependente">
+    <select name="tipo-modificador" id="tipo-modificador-${poder}-${efeito}-${ligado}-${contadorModificadores[poder][efeito][ligado]}" class="dependente">
       <option value="por-nivel">Por Nível</option>
       <option value="fixo-por-nivel">Fixo por Nível</option>
       <option value="fixo">Fixo</option>
     </select>
-    <input type="text" id="nome-modificador-${poder}-${efeito}-${ligado}-${contadorModificadores[poder][efeito]}" name="nome-modificador" placeholder="Modificador ${contadorModificadores[poder][efeito][ligado]} do efeito ${efeito}">
+    <input type="text" id="nome-modificador-${poder}-${efeito}-${ligado}-${contadorModificadores[poder][efeito][ligado]}" name="nome-modificador" placeholder="Modificador ${contadorModificadores[poder][efeito][ligado]} do efeito ${efeito}">
   `;
   
   listaModificadores.appendChild(novaLinha);
